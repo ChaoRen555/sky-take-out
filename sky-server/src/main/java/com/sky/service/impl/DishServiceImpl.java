92f6c3a071d67c4af1c052481e4cb4bd1958c424
rice <chihoohoo88@gmail.com>
2025-07-10 16:12:53 +1200

    Dishes Browsing Feature on Mobile Devices (WeChat)

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -125,5 +126,27 @@ public class DishServiceImpl implements DishService {
             });
             dishFlavorMapper.insertBath(flavors);
         }
+    }
+
+    /**
+     * Additionally query dishes and flavors
+     */
+    public List<DishVO> listWithFlavor(Dish dish) {
+        List<Dish> dishList = dishMapper.list(dish);
+
+        List<DishVO> dishVOList = new ArrayList<>();
+
+        for (Dish d : dishList) {
+            DishVO dishVO = new DishVO();
+            BeanUtils.copyProperties(d,dishVO);
+
+            //Get associated flavors by dish id
+            List<DishFlavor> flavors = dishFlavorMapper.getById(d.getId());
+
+            dishVO.setFlavors(flavors);
+            dishVOList.add(dishVO);
+        }
+
+        return dishVOList;
     }
 }
