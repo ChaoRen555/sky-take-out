d3899e75df926630bb0b5606abffd8e899598f79
rice <chihoohoo88@gmail.com>
2025-07-11 18:06:16 +1200

    Redis: Store frequently used dish data in Redis

--- sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,17 +21,30 @@ import java.util.List;
 public class DishController {
     @Autowired
     private DishService dishService;
-
+    @Autowired
+    private RedisTemplate redisTemplate;
     /**
-     * Query dishes by id
+     * Query dishes by Category id
      */
     @GetMapping("/list")
     public Result<List<DishVO>> list(Long categoryId) {
+        //Construct redis key using the rule: dish_categoryId
+        String key = "dish_" + categoryId;
+
+        //Check whether the specific data exists in redis
+        List<DishVO> list = (List<DishVO>) redisTemplate.opsForValue().get(key);
+        if (list !=null && list.size() > 0) {
+            return Result.success(list);
+        }
+
+
         Dish dish = new Dish();
         dish.setCategoryId(categoryId);
         dish.setStatus(StatusConstant.ENABLE);
 
-        List<DishVO> list = dishService.listWithFlavor(dish);
+          //If it doesn't exist, query from database and store it in redis then
+         list = dishService.listWithFlavor(dish);
+         redisTemplate.opsForValue().set(key,list);
 
         return Result.success(list);
     }
